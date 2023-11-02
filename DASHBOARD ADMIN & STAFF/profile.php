b8464ae3a8370ad59607c6561931a24a32a84fef
ANGIE ONG <122333867+aoaq1121@users.noreply.github.com>
2023-11-03 05:17:59 +0800

    EDIT PROFILE UI

--- DASHBOARD ADMIN & STAFF/profile.php
+++ DASHBOARD ADMIN & STAFF/profile.php
@@ -167,16 +167,16 @@
 					<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 mb-30">
 						<div class="pd-20 card-box height-100-p">
 							<div class="profile-photo">
-								<a href="modal" data-toggle="modal" data-target="#modal" class="edit-avatar"><i class="fa fa-pencil"></i></a>
+								<!--<a href="modal" data-toggle="modal" data-target="#modal" class="edit-avatar"><i class="fa fa-pencil"></i></a>-->
 								<img src="vendors/images/profilelogo.png" alt="" class="avatar-photo" />
 								<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
 									<div class="modal-dialog modal-dialog-centered" role="document">
 										<div class="modal-content">
-											<div class="modal-body pd-5">
+											<!--<div class="modal-body pd-5">
 												<div class="img-container">
 													<img id="image" src="vendors/images/photo2.jpg" alt="Picture" />
 												</div>
-											</div>
+											</div>-->
 											<div class="modal-footer">
 												<input type="submit" value="Update" class="btn btn-primary" />
 												<button type="button" class="btn btn-default" data-dismiss="modal">
